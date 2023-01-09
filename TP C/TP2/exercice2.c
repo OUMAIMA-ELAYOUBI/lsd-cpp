c72967154e8630e8b3cd3f4c93303fcf5555990a
OUMAIMA-ELAYOUBI <92079677+OUMAIMA-ELAYOUBI@users.noreply.github.com>
2023-01-09 21:19:02 +0100

    Add files via upload

---
+++ TP C/TP2/exercice2.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int **dummy(int**x,int n)
+{
+    int **y;
+    int i;
+    int j;
+    i=0;
+    y=(int**)malloc(sizeof (int*)*(n+1));
+    while (i<n)
+    {
+        y[i]=(int*)malloc(sizeof(int)*n);
+        j=0;
+        while (j<n)
+        {
+            y[i][j]=j;
+            j++;
+        }
+        i++;
+    }
+    y[n]=x[0];
+    for(int i = 1; i < n; i++)
+         free(x[i]);
+    free(x);
+    return y;
+}
+
+
+int main()
+{
+    int n, i, j;
+    int** x;
+    n=10;
+    i=0;
+    x=(int**)malloc(sizeof(int*)* n);
+    while(i<n)
+    {
+        x[i]=(int*)malloc(sizeof(int)*n);
+        j=0;
+        while(j<n)
+        {
+            x[i][j]=j;
+            j++;
+        }
+        i++;
+    }
+    x=dummy(x,n);
+    i=0;
+    while(i<n+1)
+    {
+        j=0;
+        while(j<n)
+        {
+            printf("%d ",x[i][j]);
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    for(int i = 0; i < n+1; i++)
+         free(x[i]);
+    free(x);
+
+
+    return 0;
+}
