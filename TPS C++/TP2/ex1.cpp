3cbe6349c7065cd33ed05b49172c6c4b42d58d56
OUMAIMA-ELAYOUBI <92079677+OUMAIMA-ELAYOUBI@users.noreply.github.com>
2023-01-09 21:02:50 +0100

    Create ex1.cpp

---
+++ TPS C++/TP2/ex1.cpp
@@ -0,0 +1,26 @@
+include <iostream>
+#include <cmath>
+using namespace std;
+
+template<typename type>
+
+
+
+type multiply(type m,int n)
+{
+    return m*n;
+}
+
+
+
+
+
+int main()
+{
+    int n;
+    float m;
+    cout<<"\n n=";
+    cin>>n;
+    cout<<"\n m=";
+    cin>>m;  
+    cout<<"\n n*m= "<<multiply(m,n)<<endl;
